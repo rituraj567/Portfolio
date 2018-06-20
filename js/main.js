e1440706acb618b10eb608d248200ce98da6ae11
Ritu <rituraj@raj.com>
2018-06-20 10:05:07 +0530

    added google analytics

--- js/main.js
+++ js/main.js
@@ -24,3 +24,14 @@ var text = new Typed(".text", {
 /*--Wow.js Effect-*/
 
 new WOW().init();
+
+
+/* Global site tag (gtag.js) - Google Analytics ---*/
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121109327-1');
+
